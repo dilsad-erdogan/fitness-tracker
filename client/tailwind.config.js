6ce82790ae695bdc4d42a38f00f798893a5c82ce
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-19 22:15:37 +0300

    client

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // İçeriği izlenecek dosya uzantıları
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
