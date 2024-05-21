0a00a9e748897ae6c415ffbe1e6b2d6af6a52d18
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 18:45:24 +0300

    hw services

--- client/src/services/heightWeight.js
+++ client/src/services/heightWeight.js
@@ -1,8 +1,73 @@
-// HW=http://localhost:3000/heightWeight
-// M=http://localhost:3000/movement
-// MT=http://localhost:3000/movementTitle
-// P=http://localhost:3000/program
-// S=http://localhost:3000/set
-// U=http://localhost:3000/user
-// UP=http://localhost:3000/userProgram
-// UR=http://localhost:3000/userRole
+import axios from 'axios';
+const HW = "http://localhost:3000/heightWeight";
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${HW}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching height weight:', error);
+        throw error;
+    }
+};
+
+const add = async (hwData) => {
+    try{
+        const response = await axios.post(`${HW}/add`, hwData);
+        return response.data;
+    } catch (error){
+        console.error('Error adding hw:', error);
+        throw error;
+    }
+};
+
+const updateHeight = async (id, height) => {
+    try{
+        const response = await axios.put(`${HW}/updateHeight/${id}`, height);
+        return response.data;
+    } catch (error){
+        console.error('Error updating height:', error);
+        throw error;
+    }
+};
+
+const updateWeight = async (id, weight) => {
+    try{
+        const response = await axios.put(`${HW}/updateWeight/${id}`, weight);
+        return response.data;
+    } catch (error){
+        console.error('Error updating height:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${HW}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting hw:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${HW}/byId/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching hw by id:', error);
+        throw error;
+    }
+};
+
+const hwServices = {
+    get,
+    add,
+    updateHeight,
+    updateWeight,
+    deleted,
+    byId
+};
+
+export default hwServices;
