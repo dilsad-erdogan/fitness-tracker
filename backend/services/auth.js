4605e514020d7468cb370814154086b4fdca7991
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 11:42:14 +0300

    auth services

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -8,4 +8,52 @@ const register = async (userData) => {
         console.error('Register error:', error);
         throw error.response.data;
     }
-}
+};
+
+const login = async (userData) => {
+    try{
+        const response = await axios.post(`${process.env.A}/login`, userData);
+        return response.data;
+    } catch (error){
+        console.error('Login error:', error);
+        throw error.response.data;
+    }
+};
+
+const login2FA = async (userData) => {
+    try{
+        const response = await axios.post(`${process.env.A}/login2FA`, userData);
+        return response.data;
+    } catch (error){
+        console.error('2FA login error:', error);
+        throw error.response.data;
+    }
+};
+
+const verify2FA = async (userData) => {
+    try{
+        const response = await axios.post(`${process.env.A}/verify2FA`, userData);
+        return response.data;
+    } catch (error){
+        console.error('2FA verify error:', error);
+        throw error.response.data;
+    }
+};
+
+const forgot = async (userData) => {
+    try{
+        const response = await axios.post(`${process.env.A}/forgot`, userData);
+        return response.data;
+    } catch (error){
+        console.error('Forgot password error:', error);
+        throw error.response.data;
+    }
+};
+
+export default {
+    register,
+    login,
+    login2FA,
+    verify2FA,
+    forgot
+};
