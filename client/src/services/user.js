5638878768c02e42d62bb52e62788c3cffc11cf2
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 19:32:21 +0300

    user services

---
+++ client/src/services/user.js
@@ -0,0 +1,75 @@
+// UP=http://localhost:3000/userProgram
+// UR=http://localhost:3000/userRole
+import axios from 'axios';
+const U = "http://localhost:3000/user";
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${U}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user:', error);
+        throw error;
+    }
+};
+
+const updateName = async (id, user) => {
+    try{
+        const response = await axios.put(`${U}/updateName/${id}`, user);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user name:', error);
+        throw error;
+    }
+};
+
+const updateEmail = async (id, user) => {
+    try{
+        const response = await axios.put(`${U}/updateEmail/${id}`, user);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user email:', error);
+        throw error;
+    }
+};
+
+const updatePassword = async (id, user) => {
+    try{
+        const response = await axios.put(`${U}/updatePassword/${id}`, user);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user password:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${U}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${U}/byId/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user by id:', error);
+        throw error;
+    }
+};
+
+const hwServices = {
+    get,
+    updateName,
+    updateEmail,
+    updatePassword,
+    deleted,
+    byId
+};
+
+export default hwServices;
