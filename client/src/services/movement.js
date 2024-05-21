a46d839838eaec31188ea85fa59c32027c50c505
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 22:56:07 +0300

    movement service

--- client/src/services/movement.js
+++ client/src/services/movement.js
@@ -1,9 +1,3 @@
-// MT=http://localhost:3000/movementTitle
-// P=http://localhost:3000/program
-// S=http://localhost:3000/set
-// U=http://localhost:3000/user
-// UP=http://localhost:3000/userProgram
-// UR=http://localhost:3000/userRole
 import axios from 'axios';
 const M = "http://localhost:3000/movement";
 
