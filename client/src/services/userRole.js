cefd9e4b6ed1d0715d6236ad0b55043192a1f0b4
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 22:11:00 +0300

    user role services

---
+++ client/src/services/userRole.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+const UR = "http://localhost:3000/userRole";
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${UR}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user role:', error);
+        throw error;
+    }
+};
+
+const add = async (urData) => {
+    try{
+        const response = await axios.post(`${UR}/add`, urData);
+        return response.data;
+    } catch (error){
+        console.error('Error adding user role:', error);
+        throw error;
+    }
+};
+
+const update = async (id, urData) => {
+    try{
+        const response = await axios.put(`${UR}/updateHeight/${id}`, urData);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user role:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${UR}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting user role:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${UR}/byId/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user role by id:', error);
+        throw error;
+    }
+};
+
+const urServices = {
+    get,
+    add,
+    update,
+    deleted,
+    byId
+};
+
+export default urServices;
