8cf46ef99dcca9cd43ed53b585b73f6d57e1bcfa
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-05-21 19:41:36 +0300

    up services

---
+++ client/src/services/userProgram.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+const UP = "http://localhost:3000/userProgram";
+
+const get = async () => {
+    try{
+        const response = await axios.get(`${UP}/get`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user program:', error);
+        throw error;
+    }
+};
+
+const add = async (upData) => {
+    try{
+        const response = await axios.post(`${UP}/add`, upData);
+        return response.data;
+    } catch (error){
+        console.error('Error adding user program:', error);
+        throw error;
+    }
+};
+
+const updateName = async (id, name) => {
+    try{
+        const response = await axios.put(`${UP}/updateName/${id}`, name);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user program:', error);
+        throw error;
+    }
+};
+
+const updateDescription = async (id, desc) => {
+    try{
+        const response = await axios.put(`${UP}/updateDescription/${id}`, desc);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user program:', error);
+        throw error;
+    }
+};
+
+const updateDuration = async (id, duration) => {
+    try{
+        const response = await axios.put(`${UP}/updateDuration/${id}`, duration);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user program:', error);
+        throw error;
+    }
+};
+
+const updateTime = async (id, time) => {
+    try{
+        const response = await axios.put(`${UP}/updateTime/${id}`, time);
+        return response.data;
+    } catch (error){
+        console.error('Error updating user program:', error);
+        throw error;
+    }
+};
+
+const deleted = async (id) => {
+    try{
+        const response = await axios.patch(`${UP}/delete/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error deleting user program:', error);
+        throw error;
+    }
+};
+
+const byId = async (id) => {
+    try{
+        const response = await axios.get(`${UP}/byId/${id}`);
+        return response.data;
+    } catch (error){
+        console.error('Error fetching user program by id:', error);
+        throw error;
+    }
+};
+
+const hwServices = {
+    get,
+    add,
+    updateName,
+    updateDescription,
+    updateDuration,
+    updateTime,
+    deleted,
+    byId
+};
+
+export default hwServices;
