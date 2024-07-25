2c55f91522c12ceb2ee53fdd1ede457447ded00c
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-07-25 19:33:22 +0300

    weekly calories router

---
+++ backend_nodejs/routes/weeklyCalorie.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getWeekly, addWeekly, updateMonday, updateTuesday, updateWednesday, updateThursday, updateFriday, updateSaturday, updateSunday, deleteWeekly, getWeeklyById } = require('../controllers/weeklyCalorie');
+
+router.route('/get').get(getWeekly);
+router.route('/add').post(addWeekly);
+router.route('/updateMonday/:id').put(updateMonday);
+router.route('/updateTuesday/:id').put(updateTuesday);
+router.route('/updateWednesday/:id').put(updateWednesday);
+router.route('/updateThursday/:id').put(updateThursday);
+router.route('/updateFriday/:id').put(updateFriday);
+router.route('/updateSaturday/:id').put(updateSaturday);
+router.route('/updateSunday/:id').put(updateSunday);
+router.route('/delete/:id').patch(deleteWeekly);
+router.route('/byId/:id').get(getWeeklyById);
+
+module.exports = router;
