853d30ec6dd20bdff185d0cae1187484c44c2156
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-07-25 19:27:12 +0300

    todolist router

---
+++ backend_nodejs/routes/todoList.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { getTodoList, addTodoList, updateContent, deleteTodoList, getTodoById } = require('../controllers/todoList');
+
+router.route('/get').get(getTodoList);
+router.route('/add').post(addTodoList);
+router.route('/update/:id').put(updateContent);
+router.route('/delete/:id').patch(deleteTodoList);
+router.route('/byId/:id').get(getTodoById);
+
+module.exports = router;
