1c67c5e28d8b99c89c5438242a74a3ef527a60ba
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-08-10 00:34:07 +0300

    program table

--- client/src/pages/admin/list/Program.jsx
+++ client/src/pages/admin/list/Program.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import '../style.scss';
+import { useEffect, useState } from "react"
+import Sidebar from '../../../components/sidebar/SidebarinAdmin';
+import Navbar from '../../../components/navbar/Navbar';
+import pServices from '../../../services/program';
 
 const Program = () => {
+  const [datas, setDatas] = useState([]);
+
+  const fetchP = async() => {
+    try{
+      const response = await pServices.get();
+      setDatas(response.data);
+    } catch(error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchP();
+  }, []);
+
   return (
-    <div>Program</div>
+    <div className='adminPanel'>
+      <Sidebar></Sidebar>
+
+      <div className='homeContainer'>
+        <Navbar></Navbar>
+
+        <div className="flex flex-col mt-5">
+          <div className="-m-1.5 overflow-x-auto">
+            <div className="p-1.5 min-w-full inline-block align-middle">
+              <div className="overflow-hidden">
+                <table className="min-w-full divide-y divide-[#405D72] dark:divide-[#758694]">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-start text-s font-medium text-[#405D72] uppercase dark:text-[#F7E7DC]">Name</th>
+                      <th scope="col" className="px-6 py-3 text-start text-s font-medium text-[#405D72] uppercase dark:text-[#F7E7DC]">Description</th>
+                      <th scope="col" className="px-6 py-3 text-end text-s font-medium text-[#405D72] uppercase dark:text-[#F7E7DC]">Duration</th>
+                      <th scope="col" className="px-6 py-3 text-end text-s font-medium text-[#405D72] uppercase dark:text-[#F7E7DC]">Time</th>
+                      <th scope="col" className="px-6 py-3 text-end text-s font-medium text-[#405D72] uppercase dark:text-[#F7E7DC]">Action</th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="divide-y divide-[#FFF8F3] dark:divide-[#758694]">
+                    {datas.map((data) => (
+                      <tr className="text-[#405D72] hover:text-[#F7E7DC] hover:bg-[#405D72] dark:text-[#F7E7DC] dark:hover:text-[#405D72] dark:hover:bg-[#F7E7DC]" key={data._id}>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">{data.p_name}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium max-w-xs truncate">{data.p_description}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">{data.duration}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">{data.time}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                          <button type="button" className="mr-2 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent focus:outline-none disabled:opacity-50 disabled:pointer-events-none">Edit</button>
+                          <button type="button" className="ml-2 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent focus:outline-none disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
